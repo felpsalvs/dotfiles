3d4de1614d3e62f4ee1c873fc4ff8c5976fd76a4
Felipe Alves <profelipealves@outlook.com>
2023-11-02 12:10:33 -0300

    added dotfiles

---
+++ .config/Code/User/globalStorage/iocave.monkey-patch/modules/main.js
@@ -0,0 +1,7 @@
+require.config({
+    paths: {
+        "monkey-generated" : "/home/felps/.config/Code/User/globalStorage/iocave.monkey-patch/modules"
+    }
+});
+
+define(["monkey-generated/entrypoint-main"], function (){});
