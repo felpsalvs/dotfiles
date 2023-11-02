51022416356ebc08715af4a51910678699c588f4
Felipe Alves <profelipealves@outlook.com>
2023-11-02 16:25:59 -0300

    update dotfiles

--- .config/Code/User/globalStorage/iocave.monkey-patch/modules/main.js
+++
@@ -1,7 +0,0 @@
-require.config({
-    paths: {
-        "monkey-generated" : "/home/felps/.config/Code/User/globalStorage/iocave.monkey-patch/modules"
-    }
-});
-
-define(["monkey-generated/entrypoint-main"], function (){});
